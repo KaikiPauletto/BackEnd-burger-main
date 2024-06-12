c1c8eccf1d7efe2754f517571f4fe02d0000a9ba
Kaiki André Pauletto <127359758+KaikiPauletto@users.noreply.github.com>
2024-06-11 21:12:36 -0300

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+
+const configDatabase = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'classen145',
+    database: 'codeburguer',
+    //url: 'postgresql://postgres:G64fEgf-125gb1DcC2CdFf-A-DCcAadc@monorail.proxy.rlwy.net:37364/railway',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
+
+export default configDatabase;
